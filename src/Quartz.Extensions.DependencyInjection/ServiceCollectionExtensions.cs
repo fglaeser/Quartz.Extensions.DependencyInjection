b3df36be5b8ec8c1a31d89bb54db90118113597a
Facundo <fglaeser@gmail.com>
2018-11-13 11:50:10 -0300

    gitflow-feature-stash: new-func

---
+++ src/Quartz.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Quartz.Extensions.DependencyInjection
+{
+  public static class ServiceCollectionExtensions
+  {
+    public static IServiceCollection AddQuartz(this IServiceCollection services)
+    {
+      return services;
+    }
+  }
+}
