bc289fda385b1de4d4ac5c907b9e87c4a84129d7
Facundo <fglaeser@gmail.com>
2018-11-14 15:46:12 -0300

    first version

--- src/Quartz.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Quartz.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz.Spi;
+using System.Linq;
 
 namespace Quartz.Extensions.DependencyInjection
 {
@@ -7,6 +10,19 @@ namespace Quartz.Extensions.DependencyInjection
   {
     public static IServiceCollection AddQuartz(this IServiceCollection services)
     {
+      services.AddSingleton<IJobFactory, QuartzJobFactory>();
+      services.AddTransient<IHostedService>(s =>
+      {
+        var jobs = s.GetServices<IJob>().Select(j => j.GetType()).ToArray();
+        return new SchedulerHostedService(s.GetService<IJobFactory>(), s.GetService<IConfiguration>(), jobs);
+      });
+      return services;
+    }
+
+    public static IServiceCollection AddJob<TJob>(this IServiceCollection services) where TJob : IJob
+    {
+      services.Add(new ServiceDescriptor(typeof(TJob), typeof(TJob), ServiceLifetime.Singleton));
+      services.AddSingleton(s => (IJob)s.GetService(typeof(TJob)));
       return services;
     }
   }
